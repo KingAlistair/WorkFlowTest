6fa6f31854fbd3735424367a76b806a7433e3890
dani <kingalistairlol@gmail.com>
2022-09-12 15:27:50 +0200

    First push

---
+++ src/test/java/com/example/WorkflowTest/WorkflowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorkflowTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
